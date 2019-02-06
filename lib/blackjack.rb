1027f29478e126091df7204cc8fd120cb030ad37
Tracy Ngo <tngo1206@gmail.com>
2019-02-06 03:35:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -65,8 +65,8 @@ end
 
 def runner
   welcome
-  card_total = initial_round
   until card_total > 21
+  card_total = initial_round
   card_total = hit?(card_total)
   display_card_total(card_total)
   end
